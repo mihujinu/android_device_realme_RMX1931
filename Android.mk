4121808df488623d7119aaffddb5841ed071493d
SheikhFaisal713 <sheikhfaisal.19jics038@jietjodhpur.ac.in>
2021-08-04 21:16:57 +0530

    subdir

--- Android.mk
+++ Android.mk
@@ -9,6 +9,7 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),RMX1931)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
